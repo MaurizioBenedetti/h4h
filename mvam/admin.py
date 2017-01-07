102043283aec64a1703b738aa940b3efeb9e632b
hostdp6 <danielh@daniels-mbp-6.enterprisenet.org>
2017-01-07 15:34:56 -0500

    more work on models

--- mvam/admin.py
+++ mvam/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from . import models
 
-# Register your models here.
-
 admin.site.register(models.Respondent, admin.ModelAdmin)
 admin.site.register(models.DeviceType, admin.ModelAdmin)
 admin.site.register(models.LocationType, admin.ModelAdmin)
--- mvam/models.py
+++ mvam/models.py
@@ -14,6 +14,7 @@ class Respondent(models.Model):
     respondent_id = models.CharField(max_length=256, primary_key=True)
     location = models.CharField(max_length=100)
     location_type = models.ForeignKey('LocationType')
+    language = models.ForeignKey('Languages')
     device_type = models.ForeignKey('DeviceType', blank=True, null=True)
     gender = models.CharField(max_length=5, choices=GENDERS, blank=True, null=True)
     age = models.IntegerField(blank=True, null=True)
@@ -25,6 +26,17 @@ class Respondent(models.Model):
 
     def __str__(self):
         return str(self.respondent_id)
+
+
+class Languages(models.Model):
+
+   language = models.CharField(max_length=20)
+
+    def __unicode__(self):
+        return unicode(self.language)
+
+    def __str__(self):
+        return str(self.language)
 
 
 class LocationType(models.Model):
@@ -58,3 +70,35 @@ class DeviceType(models.Model):
 
     def __str__(self):
         return str(self.device_type)
+
+
+class Response(models.Model):
+
+    time = models.DateTimeField(auto_now_add=True)
+    respondent = models.ForeignKey('Respondent')
+    raw_response = models.CharField(max_length=500)
+    language = models.ForeignKey('Language')
+    question = models.ForeignKey('Question')
+    survey = models.ForeignKey('Survey')
+    session_id = models.CharField(max_length=256)
+
+
+class Question(models.Model):
+
+    pass
+
+
+class Survey(models.Model):
+
+    pass
+
+
+class Metric(models.Model):
+
+    pass
+
+class MetricResponse(models.Model):
+
+    response = models.ForeignKey('Response')
+    metric = models.ForeignKey('Metric')
+    value = models.DecimalField()
