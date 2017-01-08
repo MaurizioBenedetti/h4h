4078c879d93132de00c3eabd779d909033575d8e
Joey C <joeyc@joeys-imac.enterprisenet.org>
2017-01-08 01:32:40 -0500

    initial functions

--- mvam/scorer.py
+++ mvam/scorer.py
@@ -1,12 +1,60 @@
 from . import models
 
 
-def score_response(question_id, response):
+def score_response(response):
     """
 
     :param question_id: the id of the SurveyQuestion
     :param response: an array of response metrics
     :return: the next SurveyQuestion or a TERMINATE message
     """
-    pass
+    # QUERY to get all SurveyQuestionRules
+        # for each SurveyQuestionRules object
+            #QUERY to get all SurveyQuestionRulesArguments objects
+            #for each SurveyQuestionRulesArguments object
+                #QUERY for Operator
+                #run operator LOGIC to determine if rule is satisfied ... set is active to true in SQRule???
+                #...find and return highest priority object
 
+
+
+    question_id = response["question"]["question_id"]
+    survey_question = models.SurveyQuestion.objects.get(question=question_id)
+
+    # QUERY to get all SurveyQuestionRules
+    all_sq_rules = models.SurveyQuestionRule.objects.filter(survey_question=survey_question)
+
+    # sift out inactive SurveyQuestionRules
+    active sq_rules = []
+    for rule in all_sq_rules:
+        if rule["is_active"]:
+            active_sq_rules.append(rule)
+
+    potentials = []
+    for sq_rule in active_sq_rules:
+        #QUERY to get all SurveyQuestionRulesArguments objects
+        sqr_arguments = models.SurveyQuestionRulesArguments.objects.filter(survey_question_rules=sq_rule)
+        for sqr_argument in sqr_arguments:
+            # QUERY the operator
+            operator = models.Operator.objects.get(args_operator=sqr_argument["args_operator"])
+
+            for metric in response["metrics"]:
+                metric_id = metric["metric_id"]
+                if metric_id == sqr_argument["args_metric"]:
+                    # this matches
+                    #run operator LOGIC to determine if rule is satisfied
+                    if eval(str(metric["value"]) + operator["operator"] + str(sqr_argument["args_value"])):
+                        potentials.append(sq_rule)
+                        
+    # sort potentials on highest rules_priority
+
+
+
+
+
+
+
+
+
+            # #QUERY to get MetricResponse value
+            # metric_response = models.MetricResponse.objects.get(metric_id=sqr_argument["args_metric"])
--- mvam/storer.py
+++ mvam/storer.py
@@ -1,12 +1,36 @@
 from . import models
+import datetime ## need to add this .. (if doing dates manually is necessary)
 
 
-def store_response(question_id, response):
+def store_response(response):
     """
 
     :param question_id: the id of the SurveyQuestion
     :param response: an array of response metrics
     :return: None
     """
+    # store Response
+    timestamp = response["timestamp"]
+    respondent_id = response["respondent"]["respondent_id"]
+    raw_response = response["raw_response"]
+    question_id = response["question"]["question_id"]
+    session_id = response["session_id"]
 
-    pass
+    survey_question = models.SurveyQuestion.objects.get(question = question_id) # query for survey where question = question_id.   do we need a try/catch??
+    survey = mosels.Survey.objects.get(survey_id = survey_question.id) # survey_question.survey_question.id???
+    #survey_question.survey = survey
+
+    res = models.Response(timestamp = timestamp, respondent = respondent_id, raw_response = raw_response,
+            question = question_id, survey = survey, session_id = session_id)
+
+    res.save()
+    # store the survey object
+
+    for metric in response["metrics"]:
+        metric_id = metric["metric_id"]
+        value = metric["score"]
+        metric_res = models.MetricResponse(timestamp = timestamp,
+                response = res.id, metric = metric_id, value = value)
+        metric_res.save()
+
+    return
