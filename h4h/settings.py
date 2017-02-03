301308046d3bc4c4961544e02297ed579fcd758d
hostdp6 <danielh@Daniels-MacBook-Pro-6.local>
2017-02-03 13:21:12 -0500

    switching db settings

--- h4h/settings.py
+++ h4h/settings.py
@@ -128,9 +128,9 @@ DATABASES = {
     }
 }
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    },
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+}
