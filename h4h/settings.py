e254b64f88c23b6113832c9b172d833b54981b29
hostdp6 <danielh@Daniels-MacBook-Pro-6.local>
2017-02-03 15:50:22 -0500

    added env variable patching

--- h4h/settings.py
+++ h4h/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+from patch_env import patch_environment
+
+if 'USER' in os.environ and os.environ['USER'] is 'ec2-user':
+    patch_environment()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
